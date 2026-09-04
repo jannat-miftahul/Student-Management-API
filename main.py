69a8948c3b0bdb908760ebd71d10cdf5710c51f2
Miftahul Jannat <jannat27120@gmail.com>
2026-09-04 21:55:57 +0600

    Implement student creation and update functionality in the API

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI, Path, HTTPException, Query, Body
+from pydantic import BaseModel, Field
+from typing import Annotated, Optional
 import json
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+class Student(BaseModel):
+    id: Annotated[str, Field(..., description="Student id of the student",example="s001")]
+    name: Annotated[str, Field(..., description="Student name")]
+    age: Annotated[int, Field(..., gt=0,lt=100, description="age of the student",example="12")]
+    student_class: Annotated[int, Field(..., gt=0,lt=13)]
+    roll: Annotated[int, Field(..., gt=0,lt=101)]
+    Math_marks: Annotated[int, Field(..., gt=0,lt=101)]
+    English_marks: Annotated[int, Field(..., gt=0,lt=101)]
+    Science_marks: Annotated[int, Field(..., gt=0,lt=101)]
+    phone: Annotated[str, Field(...,example="01700000000")]
+
+
+class StudentUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None)]
+    student_class: Annotated[Optional[int], Field(default=None)]
+    roll: Annotated[Optional[int], Field(default=None)]
+    Math_marks: Annotated[Optional[int], Field(default=None)]
+    English_marks: Annotated[Optional[int], Field(default=None)]
+    Science_marks: Annotated[Optional[int], Field(default=None)]
+    phone: Annotated[Optional[str], Field(default=None)]
+
 
 def load_data():
     with open('students.json','r') as f:
@@ -41,7 +66,7 @@ def view_student_by_id(student_id: str = Path(..., description="Student id of th
 @app.get("/sort")
 def view_sorted_students(sorted_by: str = Query(..., description="Sort on the basis of class, age, roll, marks"), order: str = Query('asc', description="choose order: asc or desc")):
     
-    valid_fields = ["age", "class", "roll", "Math marks", "English marks", "Science marks",]
+    valid_fields = ["age", "student_class", "roll", "Math_marks", "English_marks", "Science_marks",]
 
     if sorted_by not in valid_fields:
         raise HTTPException(status_code=404, detail=f'Invalid field, select from {valid_fields}')
@@ -63,14 +88,30 @@ def view_sorted_students(sorted_by: str = Query(..., description="Sort on the ba
 
 
 @app.post("/create")
-def create_student(student: dict = Body()):
+def create_student(student: Student):
 
     data = load_data()
 
-    student_id = student["id"]
-    data[student_id] = student
-    del data[student_id]["id"]
+    if student.id in data:
+        raise HTTPException(status_code=400, detail="Student id already exists")
+
+    data[student.id] = student.model_dump(exclude=["id"])
 
     save_data(data)
 
-    return "Successfully student created"
+    return JSONResponse(status_code=201, content={'message': 'Student created successfully'})
+
+
+@app.put("/edit/{student_id}")
+def update_student(student_id: str, student: StudentUpdate):
+
+    data = load_data()
+
+    if student_id not in data:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    data[student_id].update(student.model_dump(exclude_unset=True))
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'Student updated successfully'})
